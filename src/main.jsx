6e198ed3fbc8f7ec12a2db688f78f2c718b042f0
Abbie <aitong0113@gmail.com>
2026-02-20 10:56:11 +0800

    add sweetalert2

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,41 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { store } from "./store.js";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
+import Swal from "sweetalert2";
+
+window.alert = (message) => {
+  let icon = "info";
+  let timer = 2000; // 預設 2 秒
+
+  if (message.includes("成功")) {
+    icon = "success";
+    timer = 2000;
+  } else if (message.includes("失敗") || message.includes("錯誤")) {
+    icon = "error";
+    timer = null; // 錯誤不自動關
+  }
+
+  Swal.fire({
+    icon,
+    title: message,
+    confirmButtonText: timer ? undefined : "確定",
+    confirmButtonColor: "#104754",
+    background: "#f8fcff",
+    color: "#104754",
+    timer,
+    timerProgressBar: timer ? true : false,
+    showConfirmButton: timer ? false : true,
+  });
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
+    <StrictMode>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
+    </StrictMode>
   </StrictMode>
 );
